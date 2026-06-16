25a9219988a82cef7aa158003ac9b59683f10070
berokwiri150-26 <berokwiri150@gmail.com>
2026-06-16 19:00:59 +0300

    More changes.

---
+++ cli-tool-lab/main.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, user_id, name):
+        self.user_id = user_id
+        self.name = name
+        self.projects = []
+
+class Project:
+    def __init__(self, project_id, name):
+        self.project_id = project_id
+        self.projects
+    
+
+class Task:
+    pass
+
